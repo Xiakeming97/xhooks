d3ff1995bcf6d89800189d7ea959a81a064f129b
XiaKeMing <xiakeming97@gmail.com>
2023-08-18 19:45:20 +0800

    :sparkles: feat(src/index.ts): 将hooks统一导出

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: XiaKeMing xiakeming97@gmail.com
+ * @Date: 2023-08-07 00:21:43
+ * @LastEditors: XiaKeMing xiakeming97@gmail.com
+ * @LastEditTime: 2023-08-18 19:43:07
+ * @FilePath: /xhooks/src/index.ts
+ * @Description: xhooks项目入口文件
+ * 
+ * Copyright (c) 2023 by XIAKEMING, All Rights Reserved. 
+ */
+import useWindowResize from './useWindowResize'
+
+export {
+  useWindowResize
+}
