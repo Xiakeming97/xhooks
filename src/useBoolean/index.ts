4fecaddb0f0e856fd6197477471d3fc904b13b47
XiaKeMing <xiakeming97@gmail.com>
2023-09-10 19:23:43 +0800

    :pencil2: docs(use_window_resize): useWindowResize 完善代码和文档

--- src/useBoolean/index.ts
+++ src/useBoolean/index.ts
@@ -1,3 +1,14 @@
+/*
+ * @Author: XiaKeMing xiakeming97@gmail.com
+ * @Date: 2023-09-09 15:57:00
+ * @LastEditors: XiaKeMing xiakeming97@gmail.com
+ * @LastEditTime: 2023-09-10 18:42:36
+ * @FilePath: /xhooks/src/useBoolean/index.ts
+ * @Description: 优雅的管理 boolean 状态的 Hook。
+ * 
+ * Copyright (c) 2023 by XIAKEMING, All Rights Reserved. 
+ */
+
 import React,{useState} from 'react'
 
 export interface Actions {
